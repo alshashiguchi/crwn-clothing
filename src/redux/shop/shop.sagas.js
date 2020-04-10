0209b2b4fec072832580132dd390fcc5ebd15cf5
André Hashiguchi <andre.hashiguchi@onclick.com.br>
2020-04-09 22:53:32 -0300

    creating a redux-saga for fetching our collections from the firestore database

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,27 @@
+import { takeLatest, call, put } from 'redux-saga/effects'; 
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import ShopActionTypes from './shop.types';
+import { fetchCollectionsSucess, fetchCollectionsFailure } from './shop.actions';
+
+export function* fetchCollectionsAsync(){
+  yield console.log('I am fired');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSucess(collectionsMap))
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart(){
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START, 
+    fetchCollectionsAsync)
+}
+
+//call - is the efect inside of our generator function that invokes the method.
+//put - saga effect for creating action.
