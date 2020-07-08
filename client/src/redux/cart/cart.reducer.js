77fef310657470fff8de89609fc54c4d24bca2d3
André Hashiguchi <andre.hashiguchi@onclick.com.br>
2020-07-08 13:51:07 -0300

    updating sagas with code to create cart based on user

--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -21,16 +21,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: removeItemFromCart(state.cartItems, action.payload)  
-      }
+      };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
         cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
-      }
+      };
     case CartActionTypes.CLEAR_CART:
       return{
         ...state,
         cartItems: []
+      };
+    case CartActionTypes.SET_CART_FROM_FIREBASE:
+      return {
+        ...state,
+        cartItems: action.payload
       };
     default:
       return state
--- client/src/redux/cart/cart.sagas.js
+++ client/src/redux/cart/cart.sagas.js
@@ -1,6 +1,11 @@
-import { all, call, takeLatest, put } from 'redux-saga/effects';
+import { all, call, takeLatest, put, select } from 'redux-saga/effects';
 import UserActionTypes from '../user/user.types';
-import { clearCart } from './cart.action';
+import { clearCart, setCartFromFirebase } from './cart.action';
+
+import { getUserCartRef } from '../../firebase/firebase.utils';
+import { selectCartItems } from './cart.selectors';
+import CartActionTypes from './cart.types'
+import { selectCurrentUser } from '../user/user.selector';
 
 export function* clearCartyOnSignOut() {
   yield put(clearCart());
@@ -10,8 +15,45 @@ export function* onSignOutSucess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartyOnSignOut);
 }
 
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurrentUser);
+
+  if(currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id);
+      const cartItems = yield select(selectCartItems);
+      yield cartRef.update({ cartItems });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase);
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [
+      CartActionTypes.ADD_ITEM,
+      CartActionTypes.REMOVE_ITEM,
+      CartActionTypes.CLEAR_ITEM_FROM_CART
+    ],
+    updateCartInFirebase
+  );
+}
+
+export function* checkCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapshot = yield cartRef.get();
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems));
+}
+
 export function* cartSagas () {
   yield all([
-    call(onSignOutSucess)
+    call(onSignOutSucess), 
+    call(onCartChange), 
+    call(onUserSignIn)
   ]);
 }
