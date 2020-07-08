b16a96917559465012ce1715e5b5d500279dd8e5
André Hashiguchi <andre.hashiguchi@onclick.com.br>
2020-07-08 11:56:58 -0300

    updating our code to be pwd compliant, added, serviceWorker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor} from './redux/store';
+import * as serviceWorker from './serviceWorker';
+
 import './index.css';
 import App from './App';
 
-import { store, persistor} from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }> 
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>, 
   document.getElementById('root')
 );
+
+serviceWorker.register();
